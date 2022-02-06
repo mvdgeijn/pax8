1f23467d52788f81de9742f4be300d249223d005
bHosted <marc@vdgeijn.com>
2022-02-06 15:31:07 +0100

    Added comments in ContactRequest.php

--- src/Requests/ContactRequest.php
+++ src/Requests/ContactRequest.php
@@ -3,12 +3,25 @@
 namespace Mvdgeijn\Pax8\Requests;
 
 use Illuminate\Support\Collection;
-use Mvdgeijn\Pax8\Responses\Company;
 use Mvdgeijn\Pax8\Responses\Contact;
 
 class ContactRequest extends AbstractRequest
 {
-    public function list( string $companyId ): ?Collection
+    /**
+     * Returns a paginated list of contacts ordered by createDate descending
+     *
+     * Possible options:
+     * - page (default 0): The page number of the request for in the contacts list
+     * - size (default 10): number of contacts per page
+     *
+     * Returns null if company not found
+     *
+     * @param string $companyId
+     * @param array $options
+     * @return Collection|null
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function list(string $companyId, array $options = [] ): ?Collection
     {
         $response = $this->getRequest( "/v1/companies/$companyId/contacts");
 
@@ -18,9 +31,36 @@ class ContactRequest extends AbstractRequest
             return null;
     }
 
-    public function get( string $companyId, string $contactId ): ?Contact
+    /**
+     * Returns a contact, identified by the companyId and contactId. Returns null
+     * if companyId and/or contactId not found
+     *
+     * @param string $companyId
+     * @param string $contactId
+     * @return Contact|null
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function get(string $companyId, string $contactId ): ?Contact
     {
         $response = $this->getRequest("/v1/companies/$companyId/contacts/$contactId" );
+
+        if ($response->getStatusCode() == 200)
+            return Contact::parseContact(json_decode( $response->getBody() ) );
+        else
+            return null;
+    }
+
+    /**
+     * Creates a new Contact. Returns a new Contact object when successfully created,
+     * or null if company not found or invalid contact information is passed.
+     *
+     * @param Contact $contact
+     * @return Contact|null
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function create(Contact $contact ): ?Contact
+    {
+        $response = $this->getRequest('/v1/companies/contacts', $contact->createContact() );
 
         if ($response->getStatusCode() == 200)
             return Contact::parseContact(json_decode( $response->getBody() ) );
