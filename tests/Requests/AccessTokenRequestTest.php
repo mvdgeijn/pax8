5221d865f6dfb994d4daa12cf3b2ba4b6db7d7df
bHosted <marc@vdgeijn.com>
2022-02-06 13:17:01 +0100

    Create AccessTokenRequest tests

--- tests/Requests/AccessTokenRequestTest.php
+++ tests/Requests/AccessTokenRequestTest.php
@@ -2,18 +2,32 @@
 
 namespace Requests;
 
-use PHPUnit\Framework\TestCase;
+use Mvdgeijn\Pax8\Requests\AccessTokenRequest;
+use Tests\TestCase;
 
 class AccessTokenRequestTest extends TestCase
 {
 
     public function testGetAccessToken()
     {
+        $accessTokenRequest = new AccessTokenRequest();
 
+        $accessToken = $accessTokenRequest->getAccessToken();
+
+        $this->assertNotNull( $accessToken );
+
+        $this->assertIsObject( $accessToken );
+
+        $this->assertNotEmpty( $accessToken->accessToken );
+
+        $this->assertTrue( $accessToken->expiryTimestamp > time() );
     }
 
     public function test__construct()
     {
+        $accessTokenRequest = new AccessTokenRequest();
 
+        /* Test if an actual accessTokenRequest is returned */
+        $this->assertNotNull($accessTokenRequest );
     }
 }
